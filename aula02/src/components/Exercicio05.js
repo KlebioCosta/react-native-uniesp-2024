d7b56c9ce4003b2d90995eee463046aa2f71cc7c
Edinailton Klébio C  Ferreira <2022211510045@iesp.edu.br>
2024-09-10 17:55:13 -0300

    inicio do desafio exercicio05

---
+++ aula02/src/components/Exercicio05.js
@@ -0,0 +1,42 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Exercicio05 = () => {
+    
+  return (
+    <View style={styles.container}>
+        <View style={styles.bodyStyle}>
+            <Text>Cabeçalho</Text>
+        </View>
+        
+        <View style={styles.bodyStyle}>
+            <Text>Área de Entrada</Text>
+        </View>
+
+        <View style={styles.listStyle}>
+            <Text>Lista de tarefs</Text>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    headerStyle: {
+        fontStyle: 'italic',
+    
+    },
+    bodyStyle: {
+
+    },
+    listStyle: {
+
+    }
+})
+
+export default Exercicio05
