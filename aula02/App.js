932fe9ea65c58a114fbc0f74449f3ddf7b0c3a6b
Edinailton Klébio C  Ferreira <2022211510045@iesp.edu.br>
2024-09-23 07:09:19 -0300

    exercicio da aula 04

--- aula02/App.js
+++ aula02/App.js
@@ -3,16 +3,17 @@ import React from 'react'
 //import Exercicio01 from './src/components/Exercicio01'
 //import Exercicio02 from './src/components/Exercicio02'
 //import Exercicio03 from './src/components/Exercicio03'
-import Exercicio04 from './src/components/Exercicio04'
+//import Exercicio04 from './src/components/Exercicio04'
+import Exercicio05 from './src/components/Exercicio05'
 
 const App = () => {
   return (
     <View style={styles.safeArea}>
       {/*<Exercicio01 />
       <Exercicio02 />
-      <Exercicio03 />*/}
-      <Exercicio04 />
-
+      <Exercicio03 />
+      <Exercicio04 />*/}
+      <Exercicio05 />
     </View>
   )
 }
--- aula02/src/components/Exercicio05.js
+++ aula02/src/components/Exercicio05.js
@@ -1,20 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import React, { useState } from 'react'
 
 const Exercicio05 = () => {
+    const [text, setText] = useState('')
+    const [tasks, setTasks] = useState([])
     
+    const handleAddTask = () => {
+        
+    }
   return (
     <View style={styles.container}>
-        <View style={styles.bodyStyle}>
-            <Text>Cabeçalho</Text>
+
+        <View style={styles.headerStyle}>
+            <Text style={styles.headerTitleStyle}>BIBLIOTECA</Text>
+        </View>
+
+        <View style={styles.subTitleStyle}>
+        <Text style={styles.headerSubStyle}>Controle de Livros</Text>
         </View>
         
         <View style={styles.bodyStyle}>
-            <Text>Área de Entrada</Text>
+            <Text>Saida de Livros</Text>
+            <TextInput
+                value={text}
+                onChangeText={setText}
+                placeholder='Digite o nome do Livro aqui...'
+                placeholderTextColor={'silver'}
+                style={styles.textInput}
+            />
         </View>
 
         <View style={styles.listStyle}>
-            <Text>Lista de tarefas</Text>
+            <Text>Livros com saida</Text>
         </View>
     </View>
   )
@@ -22,17 +39,38 @@ const Exercicio05 = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
 
     },
     headerStyle: {
+        justifyContent: 'center',
+        backgroundColor: 'black',
+        height: 90,
+        alignItems: 'center',
+
+    },
+    headerTitleStyle: {
+        textAlign: 'center',
+        fontSize: 26,
+        fontFamily: 'serif',
         fontStyle: 'italic',
-    
+        color: 'red',
+    },
+    subTitleStyle: {
+        alignItems: 'center',
+        margin: 20
+    },
+    headerSubStyle: {
+        fontSize: 20,
+        alignItems: 'center',
+        color: 'red',
     },
     bodyStyle: {
+        marginTop: 30,
 
+    },
+    textInput: {
+        borderColor: 'red'
     },
     listStyle: {
 
